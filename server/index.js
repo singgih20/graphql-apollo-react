1d1a152ddedf00dcbde4ea7a3375a5c54a1f44cc
Singgih Aji <sing.sasongko@endeavorpal.com>
2021-06-08 17:32:10 +0700

    server side (express)

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 6969;
+const { graphqlHTTP } = require("express-graphql");
+const schema = require('./Schemas')
+
+
+
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
+
+app.listen(PORT, () => {
+    console.log("Server Running");
+});
