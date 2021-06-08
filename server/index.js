447a410293d3c6a093b2bb081cdeebf7a3ecaba7
Singgih Aji <sing.sasongko@endeavorpal.com>
2021-06-08 19:19:11 +0700

    add cors on server

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 6969;
 const { graphqlHTTP } = require("express-graphql");
 const schema = require('./Schemas')
+const cors = require("cors");
 
-
-
+app.use(cors());
 
 app.use(
     "/graphql",
